0dcc6902e56972640446246c7b4a62ac79b35ffa
thakkarnetram <thakkarnetram10@gmail.com>
2023-11-24 01:02:58 +0530

    UserPlayer class updated

--- app/src/main/java/com/example/project1/Constants/AppConstants.java
+++ app/src/main/java/com/example/project1/Constants/AppConstants.java
@@ -1,4 +1,50 @@
 package com.example.project1.Constants;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+import com.example.project1.BitMap.BitMapBank;
+import com.example.project1.Engine.GameEngine;
+
 public class AppConstants {
+    static BitMapBank bitMapBank;
+    public static int SCREEN_WIDTH, SCREEN_HEIGHT;
+    static int gravity;
+    static int VELOCITY_JUMP;
+    static GameEngine gameEngine;
+
+    public static void initialization(Context context) {
+        setScreenSize(context);
+//        bitMapBank = new BitMapBank(context.getResources()); // TODO Constructor for BitMapBank
+        gameEngine = new GameEngine();
+        AppConstants.gravity = 2;
+        // -30
+        AppConstants.VELOCITY_JUMP = 30;
+    }
+
+    public static BitMapBank getBitMapBank() {
+        return bitMapBank;
+    }
+
+    public static GameEngine gameEngine() {
+        return gameEngine;
+    }
+
+    public static void setScreenSize(Context context) {
+        // getting the context (info) from device
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        // getting the default display size
+        Display display = windowManager.getDefaultDisplay();
+        // getting the metrics
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        // adding the metrics in the display
+        display.getMetrics(displayMetrics);
+        // adding values to variables
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+        AppConstants.SCREEN_WIDTH = width;
+        AppConstants.SCREEN_HEIGHT = height;
+    }
 }
--- app/src/main/java/com/example/project1/Player/UserPlayer.java
+++ app/src/main/java/com/example/project1/Player/UserPlayer.java
@@ -1,4 +1,53 @@
 package com.example.project1.Player;
 
+import com.example.project1.Constants.AppConstants;
+
 public class UserPlayer {
+    private int playerX , playerY , playerCurrentFrame , playerVelocity;
+    public static int maxFrame;
+    public UserPlayer() {
+        // TODO GetPlayerWidth & Height To Be Done In BitMapBank
+        playerX = AppConstants.SCREEN_WIDTH/2 - AppConstants.getBitMapBank().getPlayerWidth()/2;
+        playerY = AppConstants.SCREEN_HEIGHT/2 - AppConstants.getBitMapBank().getPlayerHeight()/2;
+        playerCurrentFrame= 0;
+        playerVelocity = 0;
+        maxFrame = 4;
+    }
+
+    public int getPlayerX() {
+        return playerX;
+    }
+
+    public void setPlayerX(int playerX) {
+        this.playerX = playerX;
+    }
+
+    public int getPlayerY() {
+        return playerY;
+    }
+
+    public void setPlayerY(int playerY) {
+        this.playerY = playerY;
+    }
+
+    public int getPlayerCurrentFrame() {
+        return playerCurrentFrame;
+    }
+
+    public void setPlayerCurrentFrame(int playerCurrentFrame) {
+        this.playerCurrentFrame = playerCurrentFrame;
+    }
+
+    public int getPlayerVelocity() {
+        return playerVelocity;
+    }
+
+    public void setPlayerVelocity(int playerVelocity) {
+        this.playerVelocity = playerVelocity;
+    }
+
+    public static int getMaxFrame() {
+        return maxFrame;
+    }
+
 }
